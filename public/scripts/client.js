b5ae02918df65a154a5ca7865a63d3edaeab045e
robotsch <christianhumble22@gmail.com>
2022-03-29 16:52:00 -0400

    refactor tweet form slide animation with toggle

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,35 +5,45 @@
  */
 
 $(document).ready(function () {
-  const tweetError = $('#tweet-error')
-  tweetError.hide()
-  const $tweetForm = $('#tweet-submit-form')
-    .submit(function (e) {
-      e.preventDefault()
-      const tweetLength = this.text.value.length
-      // Validate tweet content
-      if(this.text.value && 
-        tweetLength > 0 && 
-        tweetLength <= 140 &&
-        this.text.value.trim() !== ''
-        ) {
-          // Tweet is valid, post it to server
-          return $.ajax({
-          url: '/tweets',
-          method: 'POST',
-          data: $(this).serialize()
-        }).then( () => {
-          // Clear text box, error, show new validated tweet
-          tweetError.slideUp()
-          $('#tweet-text').val('')
-          $.ajax('/tweets', { method: 'GET'})
-            .then(function (results) {
-              renderTweets([results[results.length-1]])
-            })
-        })
-      }
-      tweetError.slideDown()
-    })
+  
+  // Hide 'optional' elements on pageload
+  const tweetError = $("#tweet-error");
+  tweetError.hide();
+  const $tweetForm = $("#tweet-submit-form");
+  $tweetForm.hide()
+
+  // Tweet form animation
+  const $composeBtn = $("#compose-btn")
+  $composeBtn.click(function() {
+    $($tweetForm).slideToggle()
+  })
+  
+  $tweetForm.submit(function (e) {
+    e.preventDefault();
+    const tweetLength = this.text.value.length;
+    // Validate tweet content
+    if (
+      this.text.value &&
+      tweetLength > 0 &&
+      tweetLength <= 140 &&
+      this.text.value.trim() !== ""
+    ) {
+      // Tweet is valid, post it to server
+      return $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize(),
+      }).then(() => {
+        // Clear text box, error, show new validated tweet
+        tweetError.slideUp();
+        $("#tweet-text").val("");
+        $.ajax("/tweets", { method: "GET" }).then(function (results) {
+          renderTweets([results[results.length - 1]]);
+        });
+      });
+    }
+    tweetError.slideDown();
+  });
 
   const createTweetElement = function (data) {
     // Dates handled by timeago jQuery plugin
@@ -51,9 +61,9 @@ $(document).ready(function () {
         ${data.user.handle}
         </div>
       </header>
-      `)
-      .append(`${safeHTML}`)
-      .append(`
+      `
+      )
+      .append(`${safeHTML}`).append(`
       <footer>
         <time>
           ${jQuery.timeago(datePosted)}
@@ -68,13 +78,12 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  const loadTweets = function() {
+  const loadTweets = function () {
     // Get all tweets, send to renderTweets
-    $.ajax('/tweets', { method: 'GET'})
-      .then(function (results) {
-        renderTweets(results)
-      })
-  }
+    $.ajax("/tweets", { method: "GET" }).then(function (results) {
+      renderTweets(results);
+    });
+  };
 
   const renderTweets = function (tweets) {
     // Grab tweet section once to avoid searching dom tree repeatedly
@@ -92,5 +101,5 @@ $(document).ready(function () {
   };
 
   // Load tweets on initial page load
-  loadTweets()
+  loadTweets();
 });
